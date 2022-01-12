b466e70348ff979ac541ba583492b9d469e30b2a
XinpingDeng <xinpingdeng@gmail.com>
2022-01-12 16:22:41 +1100

    get ifdef to header file

--- src/caobs_main/refvap.h
+++ src/caobs_main/refvap.h
@@ -1,6 +1,13 @@
+#ifndef REFVAP_H
+#define REFVAP_H
+
+#include <complex.h>
+
 #define NL 30
 
 float complex refvap(const float nu,
 		     const float t,
 		     const float pdry,
 		     const float pvap);
+
+#endif
